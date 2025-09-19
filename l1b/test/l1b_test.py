1aa00e70351631263e25bb03a521acd382dfc97b
100429586 <100429586@alumnos.uc3m.es>
2025-09-19 12:47:52 +0200

    Test 1

--- l1b/test/l1b_test.py
+++ l1b/test/l1b_test.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#poner los plots que piden
+#read all necessary files
 from common.io.writeToa import readToa
 l1b_toa0=readToa(r'C:\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-L1B\myoutputs','l1b_toa_VNIR-0.nc')
 l1b_toa1=readToa(r'C:\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-L1B\myoutputs','l1b_toa_VNIR-1.nc')
@@ -23,20 +23,25 @@ l1b_toa1_noeq=readToa(r'C:\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS
 l1b_toa2_noeq=readToa(r'C:\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-L1B\myoutputs_noeq','l1b_toa_VNIR-2.nc')
 l1b_toa3_noeq=readToa(r'C:\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-L1B\myoutputs_noeq','l1b_toa_VNIR-3.nc')
 
+#Test 1
+def diff_band(l1b_toa, l1b_toa_original):
+    tol = 0.01
+    diff= np.abs(l1b_toa_original - l1b_toa) / np.maximum(l1b_toa_original,1e-12) * 100
+    mu = np.mean(diff)
+    sigma = np.std(diff)
+    threshold = mu + 3 * sigma
+    ok = threshold < tol
+    return threshold, ok
+threshold0, ok0 = diff_band(l1b_toa0 , l1b_toa0_original)
+threshold1, ok1 = diff_band(l1b_toa1 , l1b_toa1_original)
+threshold2, ok2 = diff_band(l1b_toa2 , l1b_toa2_original)
+threshold3, ok3 = diff_band(l1b_toa3 , l1b_toa3_original)
+print("Band 0: threshold=", threshold0, ". below 0.01%?=", ok0)
+print("Band 1: threshold=", threshold1, ". below 0.01%?=", ok1)
+print("Band 2: threshold=", threshold2, ". below 0.01%?=", ok2)
+print("Band 3: threshold=", threshold3, ". below 0.01%?=", ok3)
 
-# Calcular diferencias relativas
-#   Diferencia relativa en porcentaje
-diff0 = (l1b_toa0 - l1b_toa0_original) / l1b_toa0_original * 100
-diff1 = (l1b_toa1 - l1b_toa1_original) / l1b_toa1_original * 100
-diff2 = (l1b_toa2 - l1b_toa2_original) / l1b_toa2_original * 100
-diff3 = (l1b_toa3 - l1b_toa3_original) / l1b_toa3_original * 100
-
-print(f"La diferencia en la banda 0 es: \n {diff0}")
-print(f"La diferencia en la banda 1 es: \n {diff1}")
-print(f"La diferencia en la banda 2 es: \n {diff2}")
-print(f"La diferencia en la banda 3 es: \n {diff3}")
-
-
+#Test 2
 x = np.arange(150)
 
 fig, axs = plt.subplots(2, 2, figsize=(12, 8))  # 2 filas, 2 columnas
@@ -59,6 +64,7 @@ for ax, (toa, isrf, title) in zip(axs.ravel(), datasets):
 plt.tight_layout()
 plt.show()
 
+#Test 3
 fig2, axs2 = plt.subplots(2, 2, figsize=(12, 8))  # 2 filas, 2 columnas
 
 datasets = [
