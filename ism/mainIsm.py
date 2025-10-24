3957361ae1a3f224ffdfba8e1f60c9bd1f0b8634
100429586 <100429586@alumnos.uc3m.es>
2025-10-24 12:40:01 +0200

    rerun with E2E parameters

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,13 +4,13 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\\Users\mesqu\OneDrive\Documentos\GitHub\test_eodp\auxiliary'
-indir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-ISM\myoutputs"
-
 #auxdir = r'C:\\Users\mesqu\OneDrive\Documentos\GitHub\test_eodp\auxiliary'
-#indir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-E2E\sgm_out"
-#outdir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-E2E\myismoutputs"
+#indir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
+#outdir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-ISM\myoutputs"
+
+auxdir = r'C:\\Users\mesqu\OneDrive\Documentos\GitHub\test_eodp\auxiliary'
+indir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-E2E\sgm_out"
+outdir = r"C:\\Users\mesqu\OneDrive\Escritorio\EODP_TER_2021\EODP-TS-E2E\myismoutputs"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
